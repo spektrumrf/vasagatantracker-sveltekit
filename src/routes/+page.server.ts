faf5fa19a961b571c1acf05c3aef1cb61ccd4a29
Jonas Westerlund <jh.westerlund@gmail.com>
2022-12-24 20:53:48 +0200

    Formatting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,12 +2,11 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async function ({ locals }) {
-	// const events = await locals.client.collection('events').getFullList();
-	const test = await locals.client.collection("events").getOne("ccmciosb4r0nlh0");
-	// const currentEvent = events.find((e) => e.year === new Date().getFullYear());
-  // if(!currentEvent) {
-      // throw error(404, "no current event found");
-  // }
-  // currentEvent.teams = undefined;
-	// return { currentEvent: JSON.parse(JSON.stringify(currentEvent)) };
+	const events = await locals.client.collection('events').getFullList();
+	const currentEvent = events.find((e) => e.year === new Date().getFullYear());
+  if(!currentEvent) {
+      throw error(404, "no current event found");
+  }
+  currentEvent.teams = undefined;
+	return { currentEvent: JSON.parse(JSON.stringify(currentEvent)) };
 };
