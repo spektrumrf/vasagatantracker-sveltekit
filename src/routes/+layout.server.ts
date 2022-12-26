4e9a37be7914d0bde1ce77c00b53433af6b81174
Jonas Westerlund <login@jonaswesterlund.fi>
2022-12-26 21:52:11 +0200

    Fixed incorrect type

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,4 @@
-import type { PageServerLoad } from '@svelte/kit';
+import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async function({ locals }) {
