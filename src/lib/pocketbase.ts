7be93610a866536ad772c90c88b925ea37d5d324
Jonas Westerlund <login@jonaswesterlund.fi>
2023-03-02 18:41:10 +0200

    Update pocketbase.ts

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,8 +1,8 @@
-import { dev } from "$app/environment";
+import { env } from "$env/dynamic/private";
 import PocketBase from "pocketbase";
 
 export async function getClient(cookie: string) {
-  const pocketbaseUrl = dev
+  const pocketbaseUrl = env.ENV === "DEV"
   	? 'https://vasagatantracker-pocketbase-dev.up.railway.app'
   	: 'https://vasagatantracker-pocketbase-prod.up.railway.app';
   const client = new PocketBase(pocketbaseUrl);
