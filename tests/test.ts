e85f87215cd6532019ba0a5d643993b9de21e1da
Jonas Westerlund <login@jonaswesterlund.fi>
2023-03-05 13:32:18 +0200

    Added tests with github actions

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test('Frontpages contains Vasagatantracker', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByText("Vasagatantracker")).toBeVisible();
 });
