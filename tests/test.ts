dfb38963c22cd28d3f139a03e80842e5e96a5872
Jonas Westerlund <login@jonaswesterlund.fi>
2023-03-05 22:43:16 +0200

    Test new folder for test reports

--- tests/test.ts
+++ tests/test.ts
@@ -1,11 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('Frontpage contains Vasagatantracker', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByText("Vasagatantracker")).toBeVisible();
-});
-
-test('Frontpage does contain team name', async ({ page }) => {
-  await page.goto('/');
+test("Should fail", async ({ page }) => {
+  await page.goto("/")
   await expect(page.getByText("Ruben")).toBeVisible();
-});
+})
