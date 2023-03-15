86a0776fcb5195bcd8f1b46a027612fb2994f449
Jonas Westerlund <jh.westerlund@gmail.com>
2023-03-15 13:40:45 +0200

    Added countdown component to frontpage

--- src/routes/[year]/+page.svelte
+++ src/routes/[year]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Info from "$lib/components/Info.svelte";
+	import Countdown from "$lib/components/Countdown.svelte";
 	import { event } from "$lib/stores";
 </script>
 
+<Countdown/>
 <Info info={$event?.info}/>
