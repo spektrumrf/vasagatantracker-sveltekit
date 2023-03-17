b8c61f0c645b06dbc25b68a47042c9f94e16be45
Jonas Westerlund <jh.westerlund@gmail.com>
2023-03-17 20:28:37 +0200

    Add chartjs and svelte-chartjs with an empty graph component

--- src/routes/[year]/statistics/+page.svelte
+++ src/routes/[year]/statistics/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import DrinkTable from "$lib/components/DrinkTable.svelte";
-import type { PageData } from "./$types";
+	import PointsGraph from "$lib/components/PointsGraph.svelte";
+  import type { PageData } from "./$types";
 
   export let data: PageData
   const drinkEntries = Object.entries(data.totalDrinks)
-  console.log(drinkEntries);
 </script>
 
 <h3 class="font-bold text-2xl mb-5">Statistik</h3>
@@ -12,3 +12,6 @@ import type { PageData } from "./$types";
 
 <h4 class="font-bold text-xl mb-5">Poängkällor</h4>
 <DrinkTable drinks={drinkEntries}/>
+
+<h4 class="font-bold text-xl mb-5">Poäng</h4>
+<PointsGraph/>
