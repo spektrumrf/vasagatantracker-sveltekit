da4071055a2698427fedd862c7eb81f03287ac47
Jonas Westerlund <jh.westerlund@gmail.com>
2023-03-23 10:44:20 +0200

    Get rid of svelte-chartjs

--- src/lib/components/PointsGraph.svelte
+++ src/lib/components/PointsGraph.svelte
@@ -1,35 +1,43 @@
 <script lang="ts">
-	import { Line } from 'svelte-chartjs';
-	import 'chart.js/auto';
+	import Chart from 'chart.js/auto';
 	import 'chartjs-adapter-date-fns';
 	import { fi } from 'date-fns/locale';
-	import { feats } from '$lib/stores';
+	import { feats, teams } from '$lib/stores';
+	import { onMount } from 'svelte';
 
-	let data;
-	$: {
-		const featsByTeam: { [key: string]: any[] } = {};
+	let points;
+	const featsByTeam: { [key: string]: any[] } = {};
 
-		$feats.forEach((f) => {
-			if (!featsByTeam[f.team]) {
-				featsByTeam[f.team] = [];
-			} else {
-				featsByTeam[f.team].push(f);
-			}
-		});
-		data = {
-			datasets: Object.keys(featsByTeam).map((team) =>
-				featsByTeam[team].map((f) => ({ x: f.created.slice(0,-5), y: f.points }))
-			)
-		};
-		console.log(data)
-	}
-	const options = {
-		scales: {
-			xAxis: { type: 'time' }
-		},
-		adapters: { date: { locale: fi } },
-		responsive: true
+	$feats.forEach((f) => {
+		if (!featsByTeam[f.team]) {
+			featsByTeam[f.team] = [];
+		} else {
+			featsByTeam[f.team].push(f);
+		}
+	});
+	const data = {
+		datasets: Object.keys(featsByTeam).map((team) => ({
+			data: featsByTeam[team].map((f) => ({ x: f.created.slice(0, -5), y: f.points })),
+			label: $teams.find(t => t.id === team).name,
+		}))
 	};
+
+	const config = {
+		type: 'line',
+		data,
+		options: {
+			scales: {
+				xAxis: { type: 'time' }
+			},
+			adapters: { date: { locale: fi } }
+		}
+	};
+	onMount(() => {
+		const ctx = points.getContext('2d');
+		let chart = new Chart(ctx, config);
+	});
 </script>
 
-<Line {data} {options} />
+{#if Object.keys(data).length !== 0}
+	<canvas bind:this={points} width={400} height={400} />
+{/if}
