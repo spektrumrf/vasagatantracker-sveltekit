1dae61f6db34a0df944def88449dca7885fa0c13
Jonas Westerlund <login@jonaswesterlund.fi>
2024-02-02 22:55:13 +0200

    Updated pocketbase endpoints to new Hetzner endpoints

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,11 +1,12 @@
-import { env } from "$env/dynamic/public";
-import PocketBase from "pocketbase";
+import { env } from '$env/dynamic/public';
+import PocketBase from 'pocketbase';
 
 export async function getClient(cookie: string) {
-  const pocketbaseUrl = env.PUBLIC_ENV === "DEV"
-  	? 'https://vasagatantracker-pocketbase-dev.up.railway.app'
-  	: 'https://vasagatantracker-pocketbase-prod.up.railway.app';
-  const client = new PocketBase(pocketbaseUrl);
-  client.authStore.loadFromCookie(cookie);
-  return client;
+	const pocketbaseUrl =
+		env.PUBLIC_ENV === 'DEV'
+			? 'https://db-dev.tracker.vasagatan.fi'
+			: 'https://db.tracker.vasagatan.fi';
+	const client = new PocketBase(pocketbaseUrl);
+	client.authStore.loadFromCookie(cookie);
+	return client;
 }
