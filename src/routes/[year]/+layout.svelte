a0111531c64fb346b0f1802a5e3a0685ad420202
Jonas Westerlund <login@jonaswesterlund.fi>
2024-03-13 23:49:08 +0200

    Fixed menu handling

--- src/routes/[year]/+layout.svelte
+++ src/routes/[year]/+layout.svelte
@@ -51,7 +51,7 @@
 		{#if $account}
 			<div class="flex-none">
 				<div class="dropdown">
-					<button class="btn btn-ghost btn-circle">
+					<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							class="h-5 w-5"
@@ -65,8 +65,11 @@
 								d="M4 6h16M4 12h16M4 18h7"
 							/></svg
 						>
-					</button>
-					<ul class="menu dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+					</div>
+					<ul
+						tabindex="0"
+						class="menu dropdown-content z-[1] mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+					>
 						<li class="w-full overflow-hidden"><span>{$account?.name || ''}</span></li>
 						<span class="divider -my-1" />
 						<li><a href={`${$page.url.origin}/${$page.params.year}/feats`}>Prestationer</a></li>
