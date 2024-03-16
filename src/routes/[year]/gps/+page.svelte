62cc095cd506d0ebc8ea126750b360f58df8b250
Jonas Westerlund <login@jonaswesterlund.fi>
2024-03-16 13:23:39 +0200

    Added leaflet to be used for live gps

---
+++ src/routes/[year]/gps/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import L from 'leaflet';
+	import { onMount } from 'svelte';
+	let map;
+	onMount(() => {
+		map = L.map('map').setView([51.505, -0.09], 13);
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(map);
+	});
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+		integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+		crossorigin=""
+	/>
+</svelte:head>
+
+<div id="map" bind:this={map}></div>
+
+<style>
+	#map {
+		height: 500px;
+	}
+</style>
