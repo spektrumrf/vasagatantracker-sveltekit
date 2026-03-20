744bc0bb879453917fd2ed685463d6ad1f03b903
Jonas Westerlund <login@jonaswesterlund.fi>
2026-03-20 15:14:59 +0200

    Use local pocketbase connection when on the server to aavoid possible dns problems

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,11 +1,18 @@
-import { PUBLIC_ENV } from "$env/static/public";
-import PocketBase from "pocketbase";
+import { PUBLIC_ENV } from '$env/static/public';
+import { browser } from '$app/environment';
+import PocketBase from 'pocketbase';
 
 export async function getClient(cookie: string) {
-  const pocketbaseUrl = PUBLIC_ENV === "DEV"
-    ? "https://db-dev.tracker.vasagatan.fi"
-    : "https://db.tracker.vasagatan.fi";
-  const client = new PocketBase(pocketbaseUrl);
-  client.authStore.loadFromCookie(cookie);
-  return client;
+	let pocketbaseUrl = PUBLIC_ENV === 'DEV' ? 'http://pocketbase:7003' : 'http://pocketbase:7001';
+
+	if (browser) {
+		pocketbaseUrl =
+			PUBLIC_ENV === 'DEV'
+				? 'https://db-dev.tracker.vasagatan.fi'
+				: 'https://db.tracker.vasagatan.fi';
+	}
+
+	const client = new PocketBase(pocketbaseUrl);
+	client.authStore.loadFromCookie(cookie);
+	return client;
 }
