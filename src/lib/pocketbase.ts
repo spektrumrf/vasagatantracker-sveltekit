8000b8362a4ece958f5eab9309fd3caced3e293d
Jonas Westerlund <login@jonaswesterlund.fi>
2026-03-20 15:25:55 +0200

    Don't use local pocketbase in dev

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,11 +1,12 @@
 import { PUBLIC_ENV } from '$env/static/public';
 import { browser } from '$app/environment';
 import PocketBase from 'pocketbase';
+import { dev } from '$app/environment';
 
 export async function getClient(cookie: string) {
 	let pocketbaseUrl = 'http://pocketbase:8080';
 
-	if (browser) {
+	if (browser || dev) {
 		pocketbaseUrl =
 			PUBLIC_ENV === 'DEV'
 				? 'https://db-dev.tracker.vasagatan.fi'
